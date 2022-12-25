560810599ab7fd21ef062ef0820f15e64dc0fd16
Vu Hoang Nguyen <vuhoangnguyen7t1@gmail.com>
2022-12-25 20:43:31 +0100

    Add ML_1m.py

---
+++ Recommendation System/MF_1m.py
@@ -0,0 +1,21 @@
+from Low_rank_Matrix_factorization import MF
+import pandas as pd
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+
+ratings_base = pd.read_csv('Recommendation System/ml-1m/ratings.dat', sep='::', names=r_cols, encoding='latin-1')
+ratings = ratings_base.to_numpy()
+
+# indices in Python start from 0
+ratings[:, :2] -= 1
+
+from sklearn.model_selection import train_test_split
+
+rate_train, rate_test = train_test_split(ratings, test_size=0.33, random_state=42)
+#print(rate_train.shape, rate_test.shape)
+
+rs = MF(rate_train, K = 2, lam = 0.1, print_every = 2, learning_rate = 2, max_iter = 10, user_based = 0)
+rs.fit()
+# evaluate on test data
+RMSE = rs.evaluate_RMSE(rate_test)
+print('Item-based MF, RMSE =', RMSE)
